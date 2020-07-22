22f32c96532eee0572f3e73333d26861c9c9c90c
sirlocust <jua56yu@hotmail.com>
2020-07-22 00:38:02 -0500

    se agrega ala vista el componente form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { FormComponent } from './clientes/form.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -8,6 +9,7 @@ const routes: Routes = [
   
   { path: 'clientes' ,  component: ClientesComponent},
   { path: 'directivas' ,  component: DirectivaComponent},
+  { path: 'clientes/form', component: FormComponent},
   { path: '', pathMatch: 'full', redirectTo: '/clientes'}
 ];
 
