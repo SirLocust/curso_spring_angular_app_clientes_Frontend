6e0801f5d393faa7d977ec01d6d218fc74e4f273
sirlocust <jua56yu@hotmail.com>
2020-08-29 03:36:04 -0500

    operador map formato uppercase en observable

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,77 +1,82 @@
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
-import { Observable, throwError } from 'rxjs';
+import { Observable , throwError  } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map, catchError } from 'rxjs/operators';
+import { map , catchError} from 'rxjs/operators';
 import Swal from 'sweetalert2';
 
+
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ClienteService {
-  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
 
-  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+  private urlEndPoint:string = 'http://localhost:8080/api/clientes'
 
-  constructor(private http: HttpClient, private router: Router) {}
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json'})
 
-  getClientes(): Observable<Cliente[]> {
+  constructor(private http: HttpClient , private router:Router) { }
+
+  getClientes(): Observable<Cliente[]>{
+    
     // return of(CLIENTES);
-    return this.http
-      .get<Cliente[]>(this.urlEndPoint)
-      .pipe(map((Response) => Response as Cliente[]));
-  }
-  create(cliente: Cliente): Observable<Cliente> {
-    return this.http
-      .post<Cliente>(this.urlEndPoint, cliente, { headers: this.httpHeaders })
-      .pipe(
-        map((response: any) => response.cliente as Cliente),
-        this.manejorError('Error al Crear cliente')
-      );
-  }
-
-  getCliente(id): Observable<Cliente> {
-    let mensajeError = 'Error Al Editar Cliente';
-    return this.http
-      .get<Cliente>(`${this.urlEndPoint}/${id}`)
-      .pipe(this.manejorError(mensajeError, true, '/clientes'));
-  }
-
-  update(cliente: Cliente): Observable<Cliente> {
-    return this.http
-      .put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {
-        headers: this.httpHeaders,
+    return this.http.get<Cliente[]>(this.urlEndPoint).pipe(
+      map( Response => {
+        let clientes =Response as Cliente[]
+        return  clientes.map( (cliente:Cliente) => {
+          cliente.nombre = cliente.nombre.toUpperCase()
+          return cliente
+        });
       })
-      .pipe(
-        map((response: any) => response.cliente as Cliente),
-        this.manejorError('Error al actulizar cliente')
-      );
+    )
+
+  
+    
   }
+  create(cliente: Cliente) : Observable<Cliente>{
+    return this.http.post<Cliente>(this.urlEndPoint , cliente , {headers: this.httpHeaders}).pipe(
+      map( (response:any) => response.cliente as Cliente ),
+      this.manejorError("Error al Crear cliente")
+    )
+ }
 
-  delete(id: Number): Observable<Cliente> {
-    return this.http
-      .delete<Cliente>(`${this.urlEndPoint}/${id}`, {
-        headers: this.httpHeaders,
-      })
-      .pipe(this.manejorError('Error al eliminar cliente'));
-  }
+ getCliente(id): Observable<Cliente>{
+   let mensajeError = "Error Al Editar Cliente"
+   return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+     this.manejorError(mensajeError,true,'/clientes')
+     
+   );
+ }
 
-  manejorError(
-    mensajeError: string,
-    redirecion?: boolean,
-    url?: string
-  ): typeof throwError {
-    return catchError((e) => {
-      if (e.status == 400) {
-        return throwError(e);
-      }
+ update(cliente: Cliente): Observable<Cliente>{
+   return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders} ).pipe(
+    map( (response:any) => response.cliente as Cliente ),
+     this.manejorError("Error al actulizar cliente")
+   )
+ }
 
-      if (redirecion) {
-        this.router.navigate([`${url}`]);
-      }
-      Swal.fire(mensajeError, e.error.mensaje, 'error');
+ delete(id: Number): Observable<Cliente>{
+   return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`,{headers: this.httpHeaders}  ).pipe(
+    this.manejorError("Error al eliminar cliente")
+   )
+ }
+
+ manejorError(mensajeError:string,redirecion?:boolean,url?:string): typeof throwError{
+  return catchError( e => {
+
+    if(e.status == 400){
       return throwError(e);
-    });
-  }
+    }
+
+    if(redirecion){
+      this.router.navigate([`${url}`])
+
+    }
+    Swal.fire(mensajeError, e.error.mensaje, "error")
+    return throwError(e)
+    
+  })
+ }
 }
+ 
